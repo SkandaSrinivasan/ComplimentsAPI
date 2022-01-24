0b6225146f7f6ff9ec173083d782a81d24abcba5
Skanda Srinivasan <srinivasan.sk@northeastern.edu>
2022-01-24 10:00:33 -0500

    Initial commit

---
+++ src/main/java/com/feelgood/complimentsapi/ComplimentsApiApplication.java
@@ -0,0 +1,13 @@
+package com.feelgood.complimentsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplimentsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplimentsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/feelgood/complimentsapi/ComplimentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feelgood.complimentsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplimentsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
